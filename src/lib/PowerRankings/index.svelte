5320b897a02f3dc55e13aeb9d2f0fda85d4a54bc
j-preim <j.preimesberger@gmail.com>
2025-09-25 16:43:24 -0500

    revert power rankings index

--- src/lib/PowerRankings/index.svelte
+++ src/lib/PowerRankings/index.svelte
@@ -1,11 +1,10 @@
 <script>
-    import {getNflState, getLeagueMatchups, getLeagueRosters, getLeagueTeamManagers, waitForAll, loadPlayers, getLeagueData} from '$lib/utils/helper';
+    import {getNflState, getLeagueRosters, getLeagueTeamManagers, waitForAll, loadPlayers, getLeagueData} from '$lib/utils/helper';
     import PowerRankingsDisplay from './PowerRankingsDisplay.svelte';
     import LinearProgress from '@smui/linear-progress';
     
     const helperPromises = waitForAll(
         getNflState(),
-        getLeagueMatchups(),
         getLeagueRosters(),
         getLeagueTeamManagers(),
         getLeagueData(),
@@ -26,7 +25,7 @@
 {#await helperPromises}
     <!-- promise is pending -->
     <div class="loading">
-        <p>Calculating current scores...</p>
+        <p>Calculating power rankings...</p>
         <LinearProgress indeterminate />
     </div>
 {:then [nflState, rostersData, leagueTeamManagers, leagueData, playersInfo]}
@@ -37,6 +36,3 @@
 	<!-- promise was rejected -->
 	<p>Something went wrong: {error.message}</p>
 {/await}
-
-
-
